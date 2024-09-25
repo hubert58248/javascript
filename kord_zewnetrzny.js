87207b8ef70e98e6603ed2652f7dafc64b1eeda6
hubert58248 <144531151+hubert58248@users.noreply.github.com>
2024-09-25 08:56:08 +0200

    Update kord_zewnetrzny.js

--- kord_zewnetrzny.js
+++ kord_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("red").bold().fontsize(7)+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("silver").bold().fontsize(7)+"<br>");
 document.write(document.lastModified)
